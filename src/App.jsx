c8f88006f124188a05cb68842fb58ea64177ffad
ddangmul <j.ddangmul@gmail.com>
2024-12-04 15:23:18 +0900

    pickedPlaces 삭제 시 로컬스토리지에서 삭제 & 새로고침 시 localtorage에서 데이터 불러와 pickedPlaces 초기값으로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,20 @@ import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
 import logoImg from "./assets/logo.png";
 import { sortPlacesByDistance } from "./loc.js";
 
+// 새로고침 시 localStorage에 저장된(이전에 선택-삭제하지 않은) 장소 정보 표시
+const savedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const savedPlaces = savedIds.map((id) => {
+  return AVAILABLE_PLACES.find((place) => place.id === id);
+});
+// 에러 해결 return 명시 : find가 반환한 값을 명시적으로 return해야 map함수가 각 id에 해당하는 장소를 배열로 반환할 수 있다.
+// filter로 find가 찾지 못한 경우 반환하는 undefined를 걸러내 오류를 방지해주는 게 좋다.(해당 앱에서는 id가 모두 일치하므로 필요없음)
+
 function App() {
+  console.log(savedIds);
+  console.log(savedPlaces);
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(savedPlaces);
   const [sortedPlaces, setSortedPlaces] = useState([]);
 
   useEffect(() => {
@@ -43,7 +53,7 @@ function App() {
       return [place, ...prevPickedPlaces];
     });
 
-    // [부수 효과] 로컬스토리지에 저장
+    // [부수 효과] 로컬스토리지에 id 저장
     const storedId = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
     if (storedId.indexOf(id) === -1) {
       localStorage.setItem("selectedPlaces", JSON.stringify([id, ...storedId]));
@@ -55,6 +65,13 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    // [부수 효과] 로컬 스토리지에서 id 삭제
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    localStorage.setItem(
+      "selectedPlaces",
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
@@ -84,6 +101,7 @@ function App() {
         <Places
           title="Available Places"
           places={sortedPlaces}
+          fallbackText="장소를 거리순으로 정렬합니다..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
